3efd01052dc4b6a5d5c8d5c4c8954272a60bf4ff
Gandhar Deshpande <gandhar.desh13@gmail.com>
2021-12-01 10:57:31 -0800

    debug to get input/output layers

--- hls4ml/model/optimizer/passes/relu_merge.py
+++ hls4ml/model/optimizer/passes/relu_merge.py
@@ -10,10 +10,9 @@ class MergeRelu(OptimizerPass):
     def transform(self, model, node):
         #Merge ReLU and Convolution layer if needed
         next_node = next((x for x in model.graph.values() if node.outputs[0] in x.inputs), None)
-        for x in next_node:
-            print("---------------------")
-            print(x.inputs)
-            print("---------------------")
+        print("---------------------")
+        print(next_node.inputs)
+        print("---------------------")
         if not node.get_output_nodes():
             print("WARNING: {} is the output layer! No rewiring performed.".format(node.name))
             model.remove_node(node, rewire=False)
