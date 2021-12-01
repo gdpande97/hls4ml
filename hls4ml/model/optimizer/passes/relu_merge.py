35046067f39e590db18ed52f7892e19bceb805c0
Gandhar Deshpande <gandhar.desh13@gmail.com>
2021-11-30 16:16:53 -0800

    removed debug log from relu merge and added debug to hls layers

--- hls4ml/model/optimizer/passes/relu_merge.py
+++ hls4ml/model/optimizer/passes/relu_merge.py
@@ -2,29 +2,9 @@ from hls4ml.model.optimizer import OptimizerPass
 
 class MergeRelu(OptimizerPass):
     def match(self, node):
-        print("-------------------------------------------------------------")
-        print("Input layer name is ")
-        print(node.get_input_node().__class__.__name__)
-        print("Layer name is ") 
-        print(node.__class__.__name__)
-        print("-------------------------------------------------------------")
-
         is_match = node.__class__.__name__ == 'Activation' and \
             (node.get_input_node().__class__.__name__ == 'Conv2D' or 
             node.get_input_node().__class__.__name__ == 'Conv2DBatchnorm')
-        if is_match:
-            print("-------------------------------------------------------------")
-            print("Merge relu matched, Layer name is ") 
-            print(node.__class__.__name__)
-            print("Input layer name is ")
-            print(node.get_input_node().__class__.__name__)
-            print("-------------------------------------------------------------")
-        else:
-            print("-------------------------------------------")
-            print("Did not match merge relu, the layers were")
-            print(node.__class__.__name__)
-            print(node.get_input_node().__class__.__name__)
-            print("-------------------------------------------")
         return is_match
 
     def transform(self, model, node):
